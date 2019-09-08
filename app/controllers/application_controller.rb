0da00cda047341cb7951eb9f5c1b82f13d553af5
Emily Lillie <emilylillie3@gmail.com>
2019-09-08 23:05:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
     erb :show 
   end 
   
-  get '/recipes/edit/:id' do 
+  get '/recipes/:id/edit' do 
     @recipe = Recipe.find_by_id(params[:id])
     erb :edit 
   end 
