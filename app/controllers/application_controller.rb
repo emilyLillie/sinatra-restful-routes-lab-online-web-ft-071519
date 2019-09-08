a55482f980b0fc4dda71fb9e44c4499e8111c8af
Emily Lillie <emilylillie3@gmail.com>
2019-09-08 23:30:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do 
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     # redirect to "/recipe/#{recipe.id}"
-    redirect to '/recipes/:id' 
+    redirect to "/recipes/#{Recipe.last.id}"
   end
   
   get '/recipes/:id' do 
