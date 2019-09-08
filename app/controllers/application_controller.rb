c6e4124befca09b9d5285a263822c6accd0b40e5
Emily Lillie <emilylillie3@gmail.com>
2019-09-08 23:02:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,9 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    @recipe.save 
     
+    redirect 
   end 
 
   
